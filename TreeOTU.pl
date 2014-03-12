0b06d63d4cbeaa69d79004da3d8cc05b58ed12ac
dongyingwu <dongyingwu@users.noreply.github.com>
2014-03-12 12:11:40 -0700

    Update TreeOTU.pl

--- TreeOTU.pl
+++ TreeOTU.pl
@@ -206,7 +206,8 @@ $class->root_to_nodeID($node1);
 return 1;
 }
 
-$class->root_to_nodeID($node1,$node2);
+##$class->root_to_nodeID($node1,$node2); replaced by the following line
+$class->root_mid_between_nodes($node1,$node2);
 
 return 1;
 } 
